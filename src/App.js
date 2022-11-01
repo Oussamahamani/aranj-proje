d618434dd75d47d7196c37257b0de1a77bb84245
oussamahamani <oussamahamani2000@gmail.com>
2022-11-01 19:36:25 -0400

    ggfg

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@
 
 function App() {
 
-  //  const addMoreToFavorites = (more,id) =>{
-  //   console.log(more.id)
-  //   let newUpdatedFavorites = favorite.map(m=>{
-  //     if(m.itemid === id){
-  //       return{
-  //         ...m,
-  //         rating:more.rating,
-  //         notes:more.notes
-  //       }
-  //     } else{
-  //       return m
-  //     }
-  //   })
-  //   favorite.map(m=> m.itemid === id ?{...m,...more}:m)
-  //   setFavorite(newUpdatedFavorites)
-  //  }
-
+  
   return (
 
     <div className="App">
